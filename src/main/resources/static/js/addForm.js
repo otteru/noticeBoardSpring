da05717ff6f0f00f86211133f3d7483cdb9fbfc8
otteru <otteru@naver.com>
2024-11-01 20:39:38 +0900

    메세지 코드 출력할려는 중

--- src/main/resources/static/js/addForm.js
+++ src/main/resources/static/js/addForm.js
@@ -22,14 +22,19 @@ document.getElementById("addForm").addEventListener("submit", function(e) {
                 }
                 return response.json();
             })
-            // .then(data => {
-            //     if(response.status == 400) {
-            //         $.each(error : data.errors) {
-            //             let errorDiv = document.querySelector("field-error " + data.error);
-            //             errorDiv.textContent = data.error.message;
-            //         }
-            //     }
-            // })
+            .then(data => {
+                if(data.errors) {
+                    data.errors.forEach(err => {
+                        let fieldName = err.field || "gloabal";
+                        let errorDiv = document.querySelector(`.field-error[data-field="${fieldName}"]`);
+                        if(errorDiv) {
+                            errorDiv.textContent = err.codes[0] || err.defaultMessage;
+                        }
+                    })
+                }else{
+                    alert('글 저장 중 오류가 발생했습니다: ' + error.message);
+                }
+            })
             .catch(error => {
                 console.error('Error:', error);
                 alert('글 수정 중 오류가 발생했습니다: ' + error.message);
@@ -39,34 +44,3 @@ document.getElementById("addForm").addEventListener("submit", function(e) {
 
 })
 
-
-// }).done(function () {
-//     alert('로그인 되었습니다.');
-//     window.location.href = '/';
-// }).fail(function (response) {
-//     console.log(response);
-//     if (response.status === 400 && response.responseJSON.errorDetails != null) {
-//         $.each(response.responseJSON.errorDetails, function (index, errorDetails) {
-//             let errorSpan =  document.querySelector(".error-" + errorDetails.field);
-//             errorSpan.textContent = errorDetails.message;
-//         });
-//     } else {
-//         alert(response.responseJSON.message);
-//     }
-
-
-
-//
-// .then(response => response.json())
-// .then(data => {
-//     if(!response.ok()){
-//         if(response.status === 400){
-//             sessionStorage.setItem('errors', JSON.stringify(data.errors));
-//             window.location.href = data.redirect;
-//         }else
-//             throw new Error("서버 응답이 실패했습니다.");
-//     }else{
-//         alert("글이 성공적으로 저장되었습니다.");
-//         window.location.href = `/noticeBoard/posts/${data.id}`;
-//     })
-// }
